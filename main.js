509074bf9fa6ea2ccd398ca0915660443012a3d1
Blake VanLandingham <blake.vanlandingham@softekinc.com>
2012-06-15 11:33:53 -0500

    Fixed tabs and added tests for when no "words" are in the input.

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 module.exports = function(input) {
-  var match, matches, words, i;
-  matches = input.match(/[A-Z\d]+(?![a-z])|[a-zA-Z][^A-Z\d\-_.]*/g);
-  words = [];
-
-  for (i = 0; i < matches.length; i++) {
-    match = matches[i];
-    words[words.length] = match[0].toUpperCase() + match.substring(1);
-  }
-  return words.join(" ");
+   var match, matches, words, i;
+   matches = input.match(/[A-Z\d]+(?![a-z])|[a-zA-Z][^A-Z\d\-_.]*/g);
+   words = [];
+   if (matches) {
+      for (i = 0; i < matches.length; i++) {
+         match = matches[i];
+         words[words.length] = match[0].toUpperCase() + match.substring(1);
+      }
+      return words.join(" ");
+   }
+   return input;
 };
